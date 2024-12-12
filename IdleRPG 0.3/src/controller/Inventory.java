d834793f0a58f0dbf688a4e6b066309ec3479964
sahalabagus <143763095+sahalabagus@users.noreply.github.com>
2024-12-12 13:17:31 +0700

    Update Inventory.java
    perubahan untuk view

--- IdleRPG 0.3/src/controller/Inventory.java
+++ IdleRPG 0.3/src/controller/Inventory.java
@@ -1,42 +1,38 @@
 package controller;
 
 import java.util.Scanner;
-import model.Item;
 import model.Player;
-
+import view.InventoryView;
 
 public class Inventory {
     private Player player;
+    private InventoryView inventoryView;
 
     public Inventory(Player player) {
         this.player = player;
+        this.inventoryView = new InventoryView();
     }
 
-    public void manageInventory(Scanner scanner){
+    public void manageInventory(Scanner scanner) {
         while (true) {
-            System.out.println("\n=== Inventory ===");
-            System.out.println("1. View Items");
-            System.out.println("0. Exit Inventory");
-            System.out.print("Select an option: ");
+            inventoryView.displayMenu();
             String choice = scanner.nextLine();
-            
+
             switch (choice) {
                 case "1":
                     displayItems();
                     break;
-                
+
                 case "0":
                     return;
+
                 default:
-                    System.out.println("Invalid option. Try again.");
+                    inventoryView.displayInvalidOption();
             }
         }
     }
 
     private void displayItems() {
-        System.out.println("\nYour Items:");
-        for (Item item : player.getInventory()) {
-            System.out.println("- " + item.getItemName() + " ( " + item.getType() + " )");
-        }
+        inventoryView.displayItems(player.getInventory());
     }
 }
