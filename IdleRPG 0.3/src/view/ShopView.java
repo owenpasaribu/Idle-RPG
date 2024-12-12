c7f28289dc3cd6607e7e7701094853aca17371b3
sahalabagus <143763095+sahalabagus@users.noreply.github.com>
2024-12-12 13:58:04 +0700

    Update ShopView.java

--- IdleRPG 0.3/src/view/ShopView.java
+++ IdleRPG 0.3/src/view/ShopView.java
@@ -3,21 +3,23 @@ package view;
 import model.Item;
 import model.Player;
 import java.util.List;
+import java.util.Scanner;
 
 public class ShopView {
 
-    // Menampilkan menu utama Shop
-    public void displayShopMenu(Player player) {
+    // Menampilkan menu utama Shop dan mendapatkan pilihan pengguna
+    public String getShopMenuChoice(Scanner scanner, Player player) {
         System.out.println("Welcome to the shop!");
         System.out.println("You have " + player.getGold() + " gold.");
         System.out.println("1. Buy Item");
         System.out.println("2. Change Fragment");
         System.out.println("0. Exit Shop");
         System.out.print("Select menu: ");
+        return scanner.nextLine();
     }
 
-    // Menampilkan daftar item yang dijual
-    public void displayItems(List<Item> items, Player player) {
+    // Menampilkan daftar item dan mendapatkan pilihan pengguna
+    public int getItemSelection(Scanner scanner, List<Item> items, Player player) {
         System.out.println("Gold: " + player.getGold());
         for (int i = 0; i < items.size(); i++) {
             Item item = items.get(i);
@@ -25,24 +27,21 @@ public class ShopView {
         }
         System.out.println("0 - Back");
         System.out.print("Select an option: ");
+        return scanner.nextInt();
     }
 
-    // Menampilkan pesan pembelian berhasil
     public void displayPurchaseSuccess(String itemName) {
         System.out.println("You bought " + itemName + "!");
     }
 
-    // Menampilkan pesan jika gold tidak mencukupi
     public void displayInsufficientGold() {
         System.out.println("You don't have enough money to buy this item.");
     }
 
-    // Menampilkan pesan pilihan tidak valid
     public void displayInvalidOption() {
         System.out.println("Invalid option. Try again.");
     }
 
-    // Menampilkan pesan untuk fitur yang belum tersedia
     public void displayChangeFragment() {
         System.out.println("Coming soon . . .");
     }
