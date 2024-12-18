ff9d7a7d9e7e0cf76c945654ed0b4ce87931d162
Nova Fajri S. <114303005+novafs@users.noreply.github.com>
2024-12-18 13:53:04 +0700

    Update Item.java

--- IdleRPG 0.3/src/model/Item.java
+++ IdleRPG 0.3/src/model/Item.java
@@ -3,12 +3,14 @@ package model;
 public class Item {
     String itemName;
     String type;
+    int levelReq;
     int level;
     int price;
 
-    public Item(String itemName, String type, int level, int price){
+    public Item(String itemName, String type, int levelReq, int level, int price){
         this.itemName = itemName;
         this.type = type;
+        this.levelReq = levelReq;
         this.level = level;
         this.price = price;
     }
@@ -18,6 +20,9 @@ public class Item {
     }
     public void setType(String type){
         this.type = type;
+    }
+    public void setLevelReq(int levelReq){
+        this.levelReq = levelReq;
     }
     public void setLevel(int level){
         this.level = level;
@@ -32,6 +37,9 @@ public class Item {
     public String getType(){
         return this.type;
     }
+    public int getLevelReq(){
+        return this.levelReq;
+    }
     public int getLevel(){
         return this.level;
     }
@@ -42,6 +50,7 @@ public class Item {
     public void printItemData() {
         System.out.println("Item Name: " + getItemName());
         System.out.println("Type: " + getType());
+        System.out.println("Level: " + getLevelReq());
         System.out.println("Level: " + getLevel());
         System.out.println("Price: " + getPrice());
     }
