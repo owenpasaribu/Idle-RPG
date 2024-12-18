2825fe9a3f8577361e953f1f29d3a8981f2e1403
Nova Fajri S. <114303005+novafs@users.noreply.github.com>
2024-12-18 13:48:56 +0700

    Add files via upload

---
+++ IdleRPG 0.3/src/model/Potion.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Potion extends Item{
+    int hpBoost;
+    int atkBoost;
+    int defBoost;
+    String effect;
+
+    public Potion(String name, String type, int levelReq, int level, int price, int hpBoost, int atkBoost, int defBoost, String effect){
+        super(name, type, levelReq, level, price);
+        this.hpBoost = hpBoost;
+        this.atkBoost = atkBoost;
+        this.defBoost = atkBoost;
+        this.effect = effect;
+    }
+
+    public void setHpBoost(int hpBoost){
+        this.hpBoost = hpBoost;
+    }
+    public void setAtkBoost(int atkBoost){
+        this.atkBoost = atkBoost;
+    }
+    public void setDefBoost(int defBoost){
+        this.defBoost = defBoost;
+    }
+    public void setEffect(String effect){
+        this.effect = effect;
+    }
+    
+    public int getHpBoost(){
+        return this.hpBoost;
+    }
+    public int getAtkBoost(){
+        return this.atkBoost;
+    }
+    public int getDefBoost(){
+        return this.defBoost;
+    }
+    public String getEffect(){
+        return this.effect;
+    }
+}
